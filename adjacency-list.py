892e70432d423271750533c14addd0501075cca4
Justin ThinkPad <justin.connolly@gmail.com>
2021-09-07 21:58:42 -0400

    bfs/shortest path

--- adjacency-list.py
+++ adjacency-list.py
@@ -5,13 +5,34 @@ def make_list(size: int):
     for i in range(size):
         nodeList = list(range(size))
         nodeList.remove(i)
-        numEdges = randint(0,4)
+        numEdges = randint(1,4)
         outEdges = sample(nodeList, numEdges)
         aList.append(outEdges)
     return aList
 
+def bfs(start: int, target: int, graph: list):
+    seen = [False] * len(graph)
+    queue = []
+    queue.append(start)
+    seen[start] = True
+    level = 1
+    while len(queue) > 0:
+        i = queue.pop(0)
+        for j in graph[i]:
+            if not seen[j]:
+                seen[j] = True
+                queue.append(j)
+            if j == target:
+                return level
+        level += 1
+        
+
+    return "No connecting path"
 
 
-if __name__ == 'main':
+
+
+if __name__ == '__main__':
     adj_list = make_list(5)
-    print(adh_list)
+    print(adj_list)
+    print(bfs(0,1,adj_list))
