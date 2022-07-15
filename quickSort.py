43d14567585f104df217c25038d39b929bd062b8
Justin Connolly <justin.connolly@gmail.com>
2022-07-15 12:20:21 -0400

    recursive quicksort

--- quickSort.py
+++ quickSort.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def quick_sort(a: int):
+def it_quick_sort(a: int):
     quickSort(a,0,len(a))
 
 def quickSort(myList: list, start: int, end: int):
@@ -26,13 +26,32 @@ def quickSort(myList: list, start: int, end: int):
     quickSort(myList, start, p - start + 1)
     quickSort(myList, q, end - (q - start))
 
+def re_quick_sort(A):
+    re_quickSort(A, 0, len(A) - 1)
+
+def re_quickSort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        re_quickSort(A, p, q - 1)
+        re_quickSort(A, q + 1, r)
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
 
     
 if __name__ == '__main__':
     size = input("Enter the size of the list to sort: ")
     myList = [randint(1,int(size)) for x in range(int(size))]
     print(myList)
-    quick_sort(myList)
+    re_quick_sort(myList)
     print(myList)
     for index, value in enumerate(myList):
         if  index > 0 and value < myList[index - 1]:
